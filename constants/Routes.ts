d828c96b427c741e47e786c65cdaf103c009043d
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-10-26 13:00:03 +0500

    Add Constant Route

--- constants/Routes.ts
+++ constants/Routes.ts
@@ -1,3 +1,10 @@
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
 
 export const DASHBOARD_URL = `${SITE_URL}/dashboard`;
+
+// Auth 
+export const LOGIN_URL = `${SITE_URL}/login`;
+export const SIGNUP_URL = `${SITE_URL}/sign-up`;
+
+// Chat 
+export const CHAT_URL = `${DASHBOARD_URL}/chat`;
