d55363234a5430a7106e679e5373ef1d9fe1eb4f
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-10-26 11:12:59 +0500

    Loading Icon

---
+++ assets/icon/LoadingIcon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LoadingIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    className="animate-spin h-4 w-4"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    {...props}
+  >
+    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z" />
+  </svg>
+);
+
+export default LoadingIcon;
