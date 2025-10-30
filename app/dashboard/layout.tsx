bea8ad2a3acf2acf60a9b299d50fe9ed21be0bba
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-10-30 16:07:54 +0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,33 +1,13 @@
-import { cookies } from "next/headers";
-import { AppSidebar } from "@/components/dashboard/app-sidebar";
-import { Separator } from "@/components/ui/separator";
 import { DarkModeToggle } from "@/components/ui/dark-mode-toggle";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-  const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true" || false;
-
   return (
-    <SidebarProvider
-      defaultOpen={defaultOpen}
-      style={
-        {
-          "--sidebar-width": "300px",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar />
-      <SidebarInset>
-        <header className="bg-background sticky top-0 flex shrink-0 items-center justify-between gap-2 border-b p-4">
-          <div className="flex items-center gap-2">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
-          </div>
-          <DarkModeToggle />
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <header className="bg-background sticky top-0 flex shrink-0 items-center justify-between gap-2 border-b px-4 py-2.5">
+        <div></div>
+        <DarkModeToggle />
+      </header>
+      <div>{children}</div>
+    </>
   );
 }
