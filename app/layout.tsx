eda7c1da6ff4ab41e6d40a444d6056f03569468c
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-10-30 16:07:59 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
-          <Toaster richColors position="top-center" />
+          <Providers>
+            {children}
+            <Toaster richColors position="top-center" />
+          </Providers>
         </ThemeProvider>
       </body>
     </html>
